d977ee515fc25d0601bb4ba7b55f51d0c51e0252
Daniel Alves <danielcompras18@gmail.com>
2024-01-04 22:06:53 -0300

    adicionei funcao de salvar os produtos no BD e caso de erro ou nao aparece mensagem na tela

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -12,6 +12,7 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -23,13 +24,35 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+        try {
+            conn = new conectaDAO().connectDB();
+
+            String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+
+            try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                pstmt.setString(1, produto.getNome());
+                pstmt.setInt(2, produto.getValor());
+                pstmt.setString(3, produto.getStatus());
+
+                
+                pstmt.executeUpdate();
+                JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!" );
+                
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null,"Erro ao cadastrar produto: " + e.getMessage());
+        } finally {
+            
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                System.err.println("Erro ao fechar a conexão: " + e.getMessage());
+            }
+        }
     }
-    
+
     public ArrayList<ProdutosDTO> listarProdutos(){
         
         return listagem;
